8ca503e5a51228c68b379c27a54f7c556723b9ce
yusufekaw <yusuf.eka001@gmail.com>
2024-01-10 15:37:38 +0700

    Create datatables-simple.js

---
+++ js/datatables-simple.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+
+    
+// https://github.com/fiduswriter/Simple-DataTables/wiki
+
+
+    
+const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        const table = new simpleDatatables.DataTable(datatablesSimple);
+
+        // Tambahkan event handler untuk submit form
+        table.on('submit', function(e, datatable) {
+            // Submit form secara normal
+            datatable.submit();
+        });
+    }
+});
